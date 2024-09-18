9c688c10562090b6a6863534e7e277679d61a5d9
unknown <kdsa3102@gmail.com>
2024-09-18 22:32:06 +0300

    add telegram notify on register new user

--- apps/api/src/telegram-auth/telegram-auth.module.ts
+++ apps/api/src/telegram-auth/telegram-auth.module.ts
@@ -2,10 +2,31 @@ import { Module } from '@nestjs/common';
 import { TelegramAuthService } from './telegram-auth.service';
 import { TelegramAuthController } from './telegram-auth.controller';
 import { UserModule } from '../user/user.module';
+import { init } from '@paralleldrive/cuid2';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
-  providers: [TelegramAuthService],
+  imports: [
+    UserModule,
+    TelegrafModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          token: config.get<string>('TELEGRAM_BOT_TOKEN'),
+          usePollUpdates: true,
+        };
+      },
+    }),
+  ],
+  providers: [
+    TelegramAuthService,
+    {
+      provide: 'EVENT_ID',
+      useValue: init({ length: 16 }),
+    },
+  ],
   controllers: [TelegramAuthController],
 })
 export class TelegramAuthModule {}
