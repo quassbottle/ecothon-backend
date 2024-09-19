5be6ecadab499c35433d0decece08a942c90d600
unknown <kdsa3102@gmail.com>
2024-09-19 03:57:54 +0300

    get all documents

--- apps/api/src/document-organization/document-organization.controller.ts
+++ apps/api/src/document-organization/document-organization.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { DocumentOrganizationService } from './document-organization.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { DocumentGetDto } from './dto/document-get.dto';
+import { DocumentsCreateDto } from './dto/documents-create.dto';
+import { DocumentEditDto } from './dto/document-edit.dto';
+import { mapToArrayResponse } from '@app/common';
 
 @ApiTags('Documents')
 @Controller('document')
@@ -9,8 +13,42 @@ export class DocumentOrganizationController {
     private readonly documentOrganizationService: DocumentOrganizationService,
   ) {}
 
-  @Get(':tax_id')
-  async getDocument(@Param('tax_id') taxId: string) {
-    return await this.documentOrganizationService.getDocument(taxId);
+  @Get(':tax_id/external')
+  async getExternalDocument(@Param('tax_id') taxId: string) {
+    return await this.documentOrganizationService.getExternalDocument(taxId);
+  }
+
+  @Get(':id')
+  async getDocument(@Param('id') id: string) {
+    return await this.documentOrganizationService.getDocument(id);
+  }
+
+  @Post()
+  async createDocument(@Body() dto: DocumentsCreateDto) {
+    return await this.documentOrganizationService.createDocument({data: dto});
+  }
+
+  @Patch()
+  async editDocument(@Body() dto: DocumentEditDto) {
+    return await this.documentOrganizationService.editDocument({data: dto});
+  }
+
+  @Get()
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'offset',
+    required: false,
+  })
+  async getAllDocuments(
+    @Query('limit', new ParseIntPipe({ optional: true })) limit?: number,
+    @Query('offset', new ParseIntPipe({ optional: true })) offset?: number,
+  ) {
+    return mapToArrayResponse(
+      await this.documentOrganizationService.findAll({ limit, offset }),
+      offset,
+    );
   }
 }
