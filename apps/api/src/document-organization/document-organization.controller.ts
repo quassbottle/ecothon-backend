efcbc859108156c79daad128dd274576516cf5b1
unknown <kdsa3102@gmail.com>
2024-09-19 04:20:58 +0300

    add guards

--- apps/api/src/document-organization/document-organization.controller.ts
+++ apps/api/src/document-organization/document-organization.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { DocumentOrganizationService } from './document-organization.service';
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
 import { DocumentGetDto } from './dto/document-get.dto';
 import { DocumentsCreateDto } from './dto/documents-create.dto';
 import { DocumentEditDto } from './dto/document-edit.dto';
 import { mapToArrayResponse } from '@app/common';
+import { AuthGuard } from '../auth/auth.guard';
+import { Roles } from '../auth/role.decorator';
 
 @ApiTags('Documents')
 @Controller('document')
@@ -13,21 +15,27 @@ export class DocumentOrganizationController {
     private readonly documentOrganizationService: DocumentOrganizationService,
   ) {}
 
+  @UseGuards(AuthGuard)
   @Get(':tax_id/external')
   async getExternalDocument(@Param('tax_id') taxId: string) {
     return await this.documentOrganizationService.getExternalDocument(taxId);
   }
 
+  @UseGuards(AuthGuard)
+  @Roles('admin')
   @Get(':id')
   async getDocument(@Param('id') id: string) {
     return await this.documentOrganizationService.getDocument(id);
   }
 
+  @UseGuards(AuthGuard)
   @Post()
   async createDocument(@Body() dto: DocumentsCreateDto) {
     return await this.documentOrganizationService.createDocument({data: dto});
   }
 
+  @UseGuards(AuthGuard)
+  @Roles('admin')
   @Patch()
   async editDocument(@Body() dto: DocumentEditDto) {
     return await this.documentOrganizationService.editDocument({data: dto});
@@ -42,6 +50,8 @@ export class DocumentOrganizationController {
     name: 'offset',
     required: false,
   })
+  @UseGuards(AuthGuard)
+  @Roles('admin')
   async getAllDocuments(
     @Query('limit', new ParseIntPipe({ optional: true })) limit?: number,
     @Query('offset', new ParseIntPipe({ optional: true })) offset?: number,
